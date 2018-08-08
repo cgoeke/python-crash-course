be93025a293e489a556e73ef7e6449e9356372d5
Christian Goeke <christianpgoeke@gmail.com>
2018-08-07 20:37:02 -0700

    exercise 2-8

---
+++ exercises/chapter-2/number_eight.py
@@ -0,0 +1,7 @@
+# 2-8. Number Eight: Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8. Be sure to enclose your operations
+# in print statements to see the results.
+print(4 + 4)
+print(12 - 4)
+print(2 * 4)
+print(64 / 8)
