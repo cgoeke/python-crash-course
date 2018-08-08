b4110412376641ff51b03b68988ebbed020545bf
Christian Goeke <christianpgoeke@gmail.com>
2018-08-07 19:24:23 -0700

    exercise 2-2

---
+++ exercises/chapter-2/simple_messages.py
@@ -0,0 +1,7 @@
+# 2-2. Simple Messages: Store a message in a variable, and print that message.
+# Then change the value of your variable to a new message, and print the new message.
+message = "Hello world!"
+print(message)
+
+message = "Hello San Diego!"
+print(message)
