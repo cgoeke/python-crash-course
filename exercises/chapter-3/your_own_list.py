45d5ceafdd94829d88fe223da36927be00b5e448
Christian Goeke <christianpgoeke@gmail.com>
2018-08-08 12:55:28 -0700

    exercise 3-3

---
+++ exercises/chapter-3/your_own_list.py
@@ -0,0 +1,14 @@
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
+cars = ['Lamborghini', 'Ferrari', 'Porsche']
+
+message = "A " + cars[0] + " is a fast car."
+print(message)
+
+message = "A " + cars[1] + " is a fast car."
+print(message)
+
+message = "A " + cars[2] + " is a fast car."
+print(message)
