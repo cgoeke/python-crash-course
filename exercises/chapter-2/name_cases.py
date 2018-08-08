1c40b4cbba0c56805c6228176a607da10cbcb9cf
Christian Goeke <christianpgoeke@gmail.com>
2018-08-07 19:26:09 -0700

    exercise 2-4

---
+++ exercises/chapter-2/name_cases.py
@@ -0,0 +1,6 @@
+# 2-4. Name Cases: Store a person's name in a variable, and then print that person's name
+# in lowercase, uppercase, and titlecase.
+name = "Christian Goeke"
+print(name.lower())
+print(name.upper())
+print(name.title())
