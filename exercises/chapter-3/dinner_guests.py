3fa89c4bc94c15cbd2ff51bad070c695c0adc61a
Christian Goeke <christianpgoeke@gmail.com>
2018-08-08 14:35:35 -0700

    exercise 3-9

---
+++ exercises/chapter-3/dinner_guests.py
@@ -0,0 +1,8 @@
+# 3-9. Dinner Guests: Working with one of the programs from Exercises 3-4
+# through 3-7 (page 46), use len() to print a message indicating the number
+# of people you are inviting to dinner.
+guests = ['Christian', 'Amy', 'Dad']
+num_guests = len(guests)
+message = str(num_guests) + " guests will be attending dinner."
+
+print(message)
