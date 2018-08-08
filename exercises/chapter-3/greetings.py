5e76fb1359c0ba42483790b0472a506c67886dbe
Christian Goeke <christianpgoeke@gmail.com>
2018-08-08 12:49:37 -0700

    exercise 3-2

---
+++ exercises/chapter-3/greetings.py
@@ -0,0 +1,14 @@
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+# printing each person’s name, print a message to them. The text of each message
+# should be the same, but each message should be personalized with the
+# person’s name.
+names = ['Amy', 'Ivaylo', 'Will']
+
+message = "Hello, " + names[0] + "."
+print(message)
+
+message = "Hello, " + names[1] + "."
+print(message)
+
+message = "Hello, " + names[2] + "."
+print(message)
