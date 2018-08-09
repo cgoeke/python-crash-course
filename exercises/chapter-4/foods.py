43034a6603ae12c3e7950fa0afaf94c078484fcf
Christian Goeke <christianpgoeke@gmail.com>
2018-08-08 22:30:24 -0700

    exercise 4-12

---
+++ exercises/chapter-4/foods.py
@@ -0,0 +1,13 @@
+# 4-12. More Loops: All versions of foods.py in this section have avoided using
+# for loops when printing to save space. Choose a version of foods.py, and
+# write two for loops to print each list of foods.
+my_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli']
+friend_foods = ['pizza', 'falafel', 'carrot cake', 'ice cream']
+
+print("My foods list: ")
+for food in my_foods:
+    print(food)
+
+print("\nFriend's foods list: ")
+for food in friend_foods:
+    print(food)
